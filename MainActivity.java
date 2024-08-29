4e2eeeee834f9885f1f25f6e951d7864b3905959
Parishar Rajbhandari <149572971+parisharrajbhandari@users.noreply.github.com>
2024-08-29 08:16:33 +0545

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,37 +1,66 @@
-package com.example.cafe;
+package com.example.navigationdemo;
 
-import static java.lang.Thread.sleep;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.navigationdemo.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        login= findViewById(R.id.login);
+        setContentView(binding.getRoot());
 
-        login.setOnClickListener(new View.OnClickListener() {
+        DrawerLayout drawerLayout=findViewById(R.id.main);
+        ImageView imageView=findViewById(R.id.navimg);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent loginIntent =new Intent(getApplicationContext(), middle.class);
-                startActivity(loginIntent);
-
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
+        binding.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId=item.getItemId();
+                if (itemId==R.id.home){
+                    ReplaceTab(new HomeFragment());
+                } else if (itemId==R.id.about) {
+                    ReplaceTab(new AboutFragment());
+                } else if (itemId==R.id.setting) {
+                    ReplaceTab(new SettingFragment());
+                }
+                return false;
+            }
+        });
+
+    }
+    private void ReplaceTab(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
